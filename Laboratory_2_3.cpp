3f6e2d089a805b209023c04f73c5300623ef2280
Vdmitrofanova <114224282+Vdmitrofanova@users.noreply.github.com>
2024-03-23 20:31:48 +0300

    Add files via upload

---
+++ Laboratory_2_3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+    int N;
+    std::cin >> N;
+
+    int number;
+    std::set<int> difNum; // Создаем множество для хранения различных чисел
+    for (int i = 0; i < N; i++)
+    {
+        std::cin >> number;
+        difNum.insert(number);
+    }
+
+    std::cout << difNum.size() << std::endl;
+
+    return 0;
+}
